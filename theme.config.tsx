3d148eff7b8bb3e5144f885acd304fcde9cb172f
ashiqtasdid <ashiqtasdid5@gmail.com>
2023-08-12 17:04:23 +0600

    Initialize

---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default {
+
+    logo: <span>Minecraft Utilities</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    }
+    // ...
+  }
