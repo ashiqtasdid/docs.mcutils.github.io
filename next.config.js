b94e6f0f012dabf3512195b7a3dd0f2cac613c84
Md. Ashiquzzaman Khan <63788643+ashiqtasdid@users.noreply.github.com>
2024-06-10 15:07:31 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
- 
+
 module.exports = withNextra({
   output: 'export',
   images: {
-        unoptimized: true
-  }
+    unoptimized: true,
+  },
+  basePath: '/documentation',  // Replace 'REPO_NAME' with your GitHub repository name
+  assetPrefix: '/documentation/',  // Replace 'REPO_NAME' with your GitHub repository name
 })
